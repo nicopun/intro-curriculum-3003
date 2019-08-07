6d84a524755d3b05b59af7aa1ddbf4593c8baa33
nicopun <niconicopun@protonmail.com>
2019-08-07 07:40:11 +0000

    トリボナッチ数列の実装

--- app.js
+++ app.js
@@ -2,11 +2,12 @@
 const memo = new Map();
 memo.set(0, 0);
 memo.set(1, 1);
+memo.set(2, 1);
 function trib(n) {
     if (memo.has(n)) {
         return memo.get(n);
     }
-    const value = trib(n - 1) + trib(n - 2);
+    const value = trib(n - 1) + trib(n - 2) + trib(n - 3);
     memo.set(n, value);
     return value;
 }
@@ -14,3 +15,8 @@ const length = 40;
 for (let i = 0; i <= length; i++) {
     console.log(trib(i));
 }
+
+/* n = 40 での実行時間
+real    0m0.243s
+user    0m0.236s
+sys     0m0.004s */
